4f14f619ca254be05facba3069a7d368c0a4b4da
Dutu2019 <erictanase2007@gmail.com>
2025-03-01 00:02:49 -0500

    update

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import filedialog
 
-import piano.pythonpiano
-
-# Creating freq:notes dictionnary
+# Create the dictionary mapping note names to frequencies
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 
          'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-# Create the dictionary mapping note names to frequencies
-# In MIDI standard, C0 corresponds to MIDI note 12.
 frequency_to_note = {}
-
 for octave in range(0, 8):  # Octaves 0 through 7
     for i, note in enumerate(notes):
         # Calculate MIDI note number. C0 is MIDI note 12.
         midi_note = 12 + octave * 12 + i
         # Calculate frequency using the formula:
-        # frequency = 440 * 2^((midi_note - 69)/12)
         freq = 440 * 2 ** ((midi_note - 69) / 12)
         # Round frequency to the nearest integer
         frequency_to_note[round(freq)] = f"{note}{octave}"
@@ -41,11 +35,14 @@ def ask_file_upload():
 file_path = ask_file_upload()
 
 # Variables for calculating FFT
-sec_to_read = 3.0
+sec_to_read = 0.3
 sr = sf.info(file_path).samplerate
 dT = 1.0 / sr
 data, sr = sf.read(file_path, int(sec_to_read * sr), always_2d=True)
 signal = data[:,0]
+
+# Time axis for original signal graph
+time = np.linspace(0, len(signal) / sr, num=len(signal))
 
 # Making the fft graph
 fft_freq = np.fft.fftfreq(len(signal), d=dT)
@@ -58,21 +55,19 @@ fft_values = fft_values[positive_indices]
 
 # Normalising magnitude [0, 1]
 top_indices = np.argsort(fft_values)[::-1]
-magnitude = np.abs(fft_values/fft_values[top_indices[0]])
+fft_values = np.abs(fft_values/fft_values[top_indices[0]])
 
-# Frequency filter function 
-present_freqs = fft_freq[np.where(magnitude>=0.3)]
+# Get top frequencies and search their corresponding note
+present_notes = ""
+present_freqs = fft_freq[np.where(fft_values>=0.3)]
 diffs = np.diff(present_freqs)
 mask = np.concatenate(([True], diffs > 2.0))
 filtered_present_freqs = present_freqs[mask]
 for key in frequency_to_note:
     for present_freq in filtered_present_freqs:
         if key-5 <= present_freq <= key+5:
-            print(key, frequency_to_note[key])
-
-
-# Plotting the graphs
-time = np.linspace(0, len(signal) / sr, num=len(signal))
+            if not f"{frequency_to_note[key]} ({key}Hz)\n" in present_notes:
+                present_notes += f"{frequency_to_note[key]} ({key}Hz)\n"
 
 # Plot the original signal
 plt.subplot(2, 1, 1)
@@ -83,10 +78,11 @@ plt.ylabel("Amplitude")
 
 # Plot the magnitude spectrum
 plt.subplot(2, 1, 2)
-plt.stem(fft_freq[:len(signal)//32], magnitude[:len(signal)//32], basefmt=" ")
+plt.stem(fft_freq[:len(signal)//32], fft_values[:len(signal)//32], basefmt=" ")
 plt.title("Frequency Domain")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
+plt.text(0.7, 0.2, present_notes)
 
 plt.tight_layout()
 plt.show()
